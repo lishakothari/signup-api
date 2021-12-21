2b0632d2ea2b961b84dab5b14192eefee1e88d23
Lisha Kothari <lishakothari02@gmail.com>
2021-12-21 11:35:24 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,111 @@
+var mongoose=require('mongoose');
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+const confiq=require('../configuration/config').get(process.env.NODE_ENV);
+const salt=10;
+
+const userSchema=mongoose.Schema({
+    firstname:{
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+
+    lastname:{
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+
+    password:{
+        type:String,
+        required: true,
+        minlength:6
+    },
+    
+    //confirm password
+    password2:{
+        type:String,
+        required: true,
+        minlength:6
+
+    },
+
+    token:{
+        type: String
+    }
+});
+
+// pre function to hash password
+userSchema.pre('save',function(next){
+    var user=this;
+    
+    if(user.isModified('password')){
+        bcrypt.genSalt(salt,function(err,salt){
+            if(err)return next(err);
+
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password=hash;
+                user.password2=hash;
+                next();
+            })
+
+        })
+    }
+    else{
+        next();
+    }
+});
+
+// compare password
+userSchema.methods.comparepassword=function(password,cb){
+    bcrypt.compare(password,this.password,function(err,isMatch){
+        if(err) return cb(next);
+        cb(null,isMatch);
+    });
+}
+
+// generate token when user logs in
+userSchema.methods.generateToken=function(cb){
+    var user =this;
+    var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
+
+    user.token=token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
+
+// find if user is logged in by token
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+
+    jwt.verify(token,confiq.SECRET,function(err,decode){
+        user.findOne({"_id": decode, "token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user);
+        })
+    })
+};
+
+//delete token when user logs out
+userSchema.methods.deleteToken=function(token,cb){
+    var user=this;
+
+    user.update({$unset : {token :1}},function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
+
+
+module.exports=mongoose.model('User',userSchema);
